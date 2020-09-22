7a6f5352ff52c7e71e7c625fa28cec2d267074c8
edu-msk3-407 <musirmonova@yandex.ru>
2020-09-22 22:00:52 +0300

    Калькулятор

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,12 +3,27 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+  TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.textView);
+
+
     }
+    public void numberclick(View view){
+        Button button = (Button) view;
+        if (textView.getText().toString().equals('0') ) {
+            textView.setText(button.getText().toString());
+        } else{
+                textView.setText(textView.getText().toString() +(button.getText().toString()));
+            }
+        }
 }
